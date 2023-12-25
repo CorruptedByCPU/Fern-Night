f47c765a420cc4f3fb78e1a4cd1a03cef3647c86
Andrzej Adamczyk <53617677+CorruptedByCPU@users.noreply.github.com>
2023-12-25 22:33:45 +0100

    Update rtc.c
    Correction of variable types, function naming, function order

--- kernel/driver/rtc.c
+++ kernel/driver/rtc.c
@@ -2,56 +2,6 @@
 Copyright (C) Andrzej Adamczyk (at https://blackdev.org/). All rights reserved.
 ==============================================================================*/
 
-uint8_t read_rtc_reg(int is_bcd) {
-	// Read raw value
-	uint8_t reg = driver_port_in_byte( DRIVER_RTC_PORT_data );
-
-	// Transform from BCD if needed
-	return is_bcd ? (reg >> 4) * 10 + (reg & 0xf) : reg;
-}
-
-uint64_t driver_rtc_time() {
-	// Read register B
-	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_STATUS_REGISTER_B );
-
-	// get current state of registry flags
-	uint8_t register_status = driver_port_in_byte( DRIVER_RTC_PORT_data );
-
-	// Check if other fields are BCD or binary
-	int is_bcd = !(register_status & DRIVER_RTC_STATUS_REGISTER_B_data_mode_binary);
-
-	// retrieve weekday
-	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_weekday );
-	uint64_t current = driver_port_in_byte( DRIVER_RTC_PORT_data );
-
-	// retrieve year
-	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_year );
-	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | read_rtc_reg( is_bcd );
-
-	// retrieve month
-	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_month );
-	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | read_rtc_reg( is_bcd );
-
-	// retrieve day
-	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_day_of_month );
-	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | read_rtc_reg( is_bcd );
-
-	// retrieve hour
-	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_hour );
-	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | read_rtc_reg( is_bcd );
-
-	// retrieve minutes
-	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_minutes );
-	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | read_rtc_reg( is_bcd );
-
-	// retrieve seconds
-	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_seconds );
-	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | read_rtc_reg( is_bcd );
-
-	// return current time
-	return current;
-}
-
 void driver_rtc_init() {
 	// state of real-time controller register
 	uint8_t register_status = EMPTY;
@@ -86,4 +36,54 @@ void driver_rtc_init() {
 	// receive pending interrupt of real-time controller
 	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_STATUS_REGISTER_C );
 	driver_port_in_byte( DRIVER_RTC_PORT_data );
+}
+
+uint64_t driver_rtc_time( void ) {
+	// Read register B
+	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_STATUS_REGISTER_B );
+
+	// get current state of registry flags
+	uint8_t register_status = driver_port_in_byte( DRIVER_RTC_PORT_data );
+
+	// check if RTC controller operating in Binary mode
+	uint8_t mode = ! ( register_status & DRIVER_RTC_STATUS_REGISTER_B_data_mode_binary );
+
+	// retrieve weekday
+	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_weekday );
+	uint64_t current = driver_port_in_byte( DRIVER_RTC_PORT_data );
+
+	// retrieve year
+	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_year );
+	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | driver_rtc_register( mode );
+
+	// retrieve month
+	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_month );
+	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | driver_rtc_register( mode );
+
+	// retrieve day
+	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_day_of_month );
+	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | driver_rtc_register( mode );
+
+	// retrieve hour
+	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_hour );
+	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | driver_rtc_register( mode );
+
+	// retrieve minutes
+	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_minutes );
+	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | driver_rtc_register( mode );
+
+	// retrieve seconds
+	driver_port_out_byte( DRIVER_RTC_PORT_command, DRIVER_RTC_REGISTER_seconds );
+	current = (current << STATIC_MOVE_AL_TO_HIGH_shift) | driver_rtc_register( mode );
+
+	// return current time
+	return current;
+}
+
+uint8_t driver_rtc_register( uint8_t mode ) {
+	// read value from RTC
+	uint8_t value = driver_port_in_byte( DRIVER_RTC_PORT_data );
+
+	// convert value from BCD to Binary if needed
+	return mode ? (value >> 4) * 10 + (value & 0x0F) : value;
 }
